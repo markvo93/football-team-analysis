6797d7a4b74f4d021a87bb5c1bb84847d30ec588
Mark <mark-vollmer@web.de>
2023-11-21 08:15:44 +0100

    changing language

--- src/football_team_analysis/utils/sorting_data.py
+++ src/football_team_analysis/utils/sorting_data.py
@@ -1,13 +1,13 @@
 def sort_dataframe_by_column(dataframe, column_name):
     """
-    Diese Funktion nimmt einen DataFrame und den Namen einer Spalte,
-    ordnet den DataFrame nach dieser Spalte und gibt den aktualisierten DataFrame zurück.
+    This function takes a DataFrame and the name of a column,
+    arranges the DataFrame ascending according to this column and returns the updated DataFrame.
     """
     if column_name in dataframe.columns:
-        sorted_dataframe = dataframe.sort_values(by=column_name, ascending=True)
+        sorted_dataframe = dataframe.sort_values(by=column_name, ascending=False)
         return sorted_dataframe
     else:
-        print(f"Die Spalte {column_name} wurde nicht im DataFrame gefunden.")
+        print(f"Column {column_name} hasn't been found in the dataframe.")
         return None
     
 
